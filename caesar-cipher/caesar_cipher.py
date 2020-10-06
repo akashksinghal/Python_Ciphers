8c08edfa1d501c59fc77d2efac8b553e7b70c32c
Jay Bhatia <jaybhatia55@yahoo.com>
2020-10-07 00:18:49 +0530

    caesar cipher added

---
+++ caesar-cipher/caesar_cipher.py
@@ -0,0 +1,26 @@
+def caesar_encrypt(realText, step):
+	outText = []
+	cryptText = []
+	
+	uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+	for eachLetter in realText:
+		if eachLetter in uppercase:
+			index = uppercase.index(eachLetter)
+			crypting = (index + step) % 26
+			cryptText.append(crypting)
+			newLetter = uppercase[crypting]
+			outText.append(newLetter)
+		elif eachLetter in lowercase:
+			index = lowercase.index(eachLetter)
+			crypting = (index + step) % 26
+			cryptText.append(crypting)
+			newLetter = lowercase[crypting]
+			outText.append(newLetter)
+	return outText
+
+code = caesar_encrypt('abc', 2)
+print()
+print(code)
+print()
